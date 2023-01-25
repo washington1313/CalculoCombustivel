b93e01c7ef9214dbecc9437b958af4356b766db1
Washinfron Fernandes <wmfmg@hotmail.com>
2023-01-24 21:09:28 -0300

    Projeto Finalizado

---
+++ CalculoCombustivel/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace CalculoCombustivel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double gasolina = 1.80;
+            double alcool = 1.00;
+            double tanque, valorFinal;
+            char combustivel;
+
+            Console.ForegroundColor= ConsoleColor.Yellow;
+            Console.WriteLine("**********************************");
+            Console.WriteLine("** Informe o Tipo de Combustivel**");
+            Console.WriteLine("****G - Gasolina |  A - Alcool****");
+            Console.WriteLine("**********************************");
+            combustivel = char.Parse( Console.ReadLine());
+            Console.ResetColor();
+
+            Console.ForegroundColor= ConsoleColor.Green;
+            Console.WriteLine("** Digite a capacidade do Tanque **");
+            tanque = double.Parse( Console.ReadLine());
+            Console.ResetColor();
+
+            if (combustivel == 'G'||combustivel == 'g') 
+            {
+                valorFinal = gasolina * tanque;
+                Console.WriteLine("Valor a ser pago: " + valorFinal);
+            }
+
+            else if (combustivel == 'A'|| combustivel == 'a') 
+            {
+                valorFinal= alcool * tanque;
+                Console.WriteLine("Valor a ser pago: " + valorFinal);
+            }
+
+            else 
+            {
+                Console.WriteLine("OPÇÃO INEXISTENTE");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
