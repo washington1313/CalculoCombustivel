3019cc0b7e18dee00555c0855a8e1d70b1b254eb
Washinfron Fernandes <wmfmg@hotmail.com>
2023-01-25 22:35:53 -0300

    Retirada de espaços no codigo

--- CalculoCombustivel/Program.cs
+++ CalculoCombustivel/Program.cs
@@ -6,44 +6,40 @@ namespace CalculoCombustivel
     {
         static void Main(string[] args)
         {
-
             double gasolina = 1.80;
             double alcool = 1.00;
             double tanque, valorFinal;
             char combustivel;
 
-            Console.ForegroundColor= ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("**********************************");
             Console.WriteLine("** Informe o Tipo de Combustivel**");
             Console.WriteLine("****G - Gasolina |  A - Alcool****");
             Console.WriteLine("**********************************");
-            combustivel = char.Parse( Console.ReadLine());
+            combustivel = char.Parse(Console.ReadLine());
             Console.ResetColor();
 
-            Console.ForegroundColor= ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("** Digite a capacidade do Tanque **");
-            tanque = double.Parse( Console.ReadLine());
+            tanque = double.Parse(Console.ReadLine());
             Console.ResetColor();
 
-            if (combustivel == 'G'||combustivel == 'g') 
+            if (combustivel == 'G' || combustivel == 'g')
             {
                 valorFinal = gasolina * tanque;
                 Console.WriteLine("Valor a ser pago: " + valorFinal);
             }
-
-            else if (combustivel == 'A'|| combustivel == 'a') 
+            else if (combustivel == 'A' || combustivel == 'a')
             {
-                valorFinal= alcool * tanque;
+                valorFinal = alcool * tanque;
                 Console.WriteLine("Valor a ser pago: " + valorFinal);
             }
-
-            else 
+            else
             {
                 Console.WriteLine("OPÇÃO INEXISTENTE");
             }
 
             Console.ReadKey();
-
         }
     }
 }
